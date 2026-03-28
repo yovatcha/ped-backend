0ef98a9e9399ba6e35ae814a6d7f117b44cead69
Vatcharamai <yoyo.rodring@gmail.com>
2026-03-29 02:34:12 +0700

    feat: add ped API routes, controller, and service for category retrieval and submission

---
+++ src/api/ped/routes/ped.js
@@ -0,0 +1,20 @@
+module.exports = {
+  routes: [
+    {
+      method: "POST",
+      path: "/ped/categories",
+      handler: "ped.getCategories",
+      config: {
+        auth: false,
+      },
+    },
+    {
+      method: "POST",
+      path: "/ped/send",
+      handler: "ped.sendPed",
+      config: {
+        auth: false,
+      },
+    },
+  ],
+};
