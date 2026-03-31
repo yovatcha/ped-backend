9c158af04428aa4b5374b6e06b63774408bea18c
Vatcharamai <yoyo.rodring@gmail.com>
2026-03-31 12:54:55 +0700

    change proxy key

--- config/server.ts
+++ config/server.ts
@@ -2,7 +2,7 @@ export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   url: env("PUBLIC_URL", "http://localhost:1337"),
-  proxy: true,  // ← add this
+  proxy: true,
   app: {
     keys: env.array("APP_KEYS"),
   },
